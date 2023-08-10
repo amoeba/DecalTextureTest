6418598622d0a237d41d3d01540d7e5200aaa1b3
Bryce Mecum <petridish@gmail.com>
2023-08-09 22:24:33 -0800

    Figure out text centering

--- DecalTextureTest/ExampleUI.cs
+++ DecalTextureTest/ExampleUI.cs
@@ -13,6 +13,8 @@ namespace DecalTextureTest
         private readonly Hud hud;
         private string message;
         private double t0;
+        private int width = 600;
+        private int height = 300;
 
         public ExampleUI(string _message)
         {
@@ -34,13 +36,12 @@ namespace DecalTextureTest
             try
             {
                 // TODO: This works, but factor out and cleanup
-                float w = 300;
-                float h = 200;
                 var io = ImGui.GetIO();
                 var v = ImGui.GetMainViewport();
+                var vs = v.Size;
                 var c = v.GetCenter();
-                ImGui.SetNextWindowPos(new Vector2(c.X - w/2, c.Y - w/2));
-                ImGui.SetNextWindowSize(new Vector2(w, h), ImGuiCond.Appearing);
+                ImGui.SetNextWindowPos(new Vector2(c.X - width / 2, vs.Y / 5));
+                ImGui.SetNextWindowSize(new Vector2(width, height), ImGuiCond.Appearing);
             }
             catch (Exception ex)
             {
@@ -56,17 +57,27 @@ namespace DecalTextureTest
                 Vector2 p0 = ImGui.GetItemRectMin();
                 Vector2 p1 = ImGui.GetItemRectMax();
                 ImDrawListPtr drawList = ImGui.GetWindowDrawList();
-                
+
                 // Text fading
                 // FIXME: Factor out most of this
                 // FIXME: Figure out if we can properly center our text (i.e. do 
                 //        (font metrics)
                 double t1 = ImGui.GetTime() - t0;
+
                 // Messy bit of code to smoothly move from 0-1 and back to 0
                 double s = PluginCore.duration_ms / 1000;
                 double t = Math.Min(Math.Max((Math.Sin(t1) * s) / s, 0.0f), 1.0f);
                 uint c = ImGui.Vec4ToCol(new Vector4(1.0f, 1.0f, 1.0f, (float)t));
-                drawList.AddText(PluginCore.font, 40, new Vector2(p0.X, p0.Y), c, message);
+
+                // Center text
+                ImGui.PushFont(PluginCore.font);
+                Vector2 text_size = ImGui.CalcTextSize(message);
+                ImGui.PopFont(); 
+                Vector2 center = p0 + (p1 - p0) / 2;
+                Vector2 offset = new Vector2(-text_size.X / 2, -text_size.Y / 2);
+
+                // Draw text
+                drawList.AddText(PluginCore.font, 40, center + offset, c, message);
             }
             catch (Exception ex)
             {
---
+++ DecalTextureTest/TextDebug.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Numerics;
+
+using ImGuiNET;
+using UtilityBelt.Service;
+using UtilityBelt.Service.Views;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DecalTextureTest
+{
+    internal class TextDebug : IDisposable
+    {
+        private readonly Hud hud;
+        private int width = 300;
+        private int height = 200;
+
+        public TextDebug()
+        {
+
+            hud = UBService.Huds.CreateHud("TextDebug");
+            hud.ShowInBar = true;
+            hud.Visible = true;
+            //hud.WindowSettings = ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoDecoration;
+
+            hud.OnRender += Hud_OnRender;
+            hud.OnPreRender += Hud_OnPreRender;
+        }
+
+        private void Hud_OnPreRender(object sender, EventArgs e)
+        {
+            try
+            {
+                var v = ImGui.GetMainViewport();
+                var c = v.GetCenter();
+                ImGui.SetNextWindowPos(new Vector2(c.X - width / 2, c.Y - width / 2));
+                ImGui.SetNextWindowSize(new Vector2(width, height), ImGuiCond.Appearing);
+            }
+            catch (Exception ex)
+            {
+                PluginCore.Log(ex);
+            }
+        }
+
+        private void Hud_OnRender(object sender, EventArgs e)
+        {
+            try
+            {
+                Vector2 size = ImGui.GetContentRegionAvail();
+                ImGui.InvisibleButton("renderbutton", size);
+                Vector2 p0 = ImGui.GetItemRectMin();
+                Vector2 p1 = ImGui.GetItemRectMax();
+                ImDrawListPtr drawList = ImGui.GetWindowDrawList();
+                drawList.PushClipRect(p0, p1);
+
+                // Do things
+                string text = "Test Message";
+
+                // Text size
+                ImGui.PushFont(PluginCore.font);
+                Vector2 text_size = ImGui.CalcTextSize(text);
+                ImGui.PopFont();
+
+                Vector2 center = p0 + (p1 - p0) / 2;
+                Vector2 offset = new Vector2(-text_size.X / 2, -text_size.Y / 2);
+                
+                // DEBUG: Find center
+                drawList.AddLine(new Vector2(p0.X + (p1.X-p0.X)/2, p0.Y), new Vector2(p0.X + (p1.X - p0.X) / 2, p1.Y), 0xFFFFFFFF);
+                drawList.AddLine(new Vector2(p0.X, p0.Y + (p1.Y - p0.Y)/2), new Vector2(p1.X, p0.Y + (p1.Y - p0.Y) / 2), 0xFFFFFFFF);
+
+                drawList.AddText(PluginCore.font, PluginCore.font.FontSize, center + offset, 0xFFFFFFFF, text);
+
+                drawList.PopClipRect();
+            }
+            catch (Exception ex)
+            {
+                PluginCore.Log(ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            hud.Dispose();
+        }
+    }
+}
